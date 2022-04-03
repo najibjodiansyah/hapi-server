37fa54a3365c672843ab97d5ed8561fe830d8789
najibjodiansyah <najibjodiansyah11@gmail.com>
2022-04-03 22:30:17 +0700

    route with method and path using * and any*

--- routes.js
+++ routes.js
@@ -7,10 +7,31 @@ const routes = [
 		},
 	},
 	{
+		method: "*",
+		path: "/",
+		handler: (request, h) => {
+			return "Halaman tidak dapat diakses dengan method tersebut";
+		},
+	},
+	{
 		method: "GET",
 		path: "/about",
 		handler: (request, h) => {
-			return "About Page";
+			return "About page";
+		},
+	},
+	{
+		method: "*",
+		path: "/about",
+		handler: (request, h) => {
+			return "Halaman tidak dapat diakses dengan method";
+		},
+	},
+	{
+		method: "*",
+		path: "/{any*}",
+		handler: (request, h) => {
+			return "Halaman tidak ditemukan";
 		},
 	},
 ];
